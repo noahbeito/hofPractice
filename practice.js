b4c49e96ffdec809bd864dac35d4b219b6d3a24d
Noah Beito <noah.beito@gmail.com>
2023-02-17 18:48:15 -0800

    pass all tests in practice.js

--- practice.js
+++ practice.js
@@ -25,12 +25,39 @@ var moreFruits = function (fruits) {
 // use _.each to traverse the number array and determine
 // which are multiples of five.
 var multiplesOfFive = function (numbers) {
+  var count = 0;
 
+  _.each(numbers, function(number, index, collection) {
+    if (number % 5 === 0) {
+      count++;
+    }
+  });
+
+  return count;
 };
 
 // use _.each to build an array containing only tweets belonging to a specified user.
-var getUserTweets = function(tweets, user) {
 
+// input: array of objects, string
+// output: array of tweets
+// constraints: use _.each
+// edge cases:
+
+// create userTweets array
+// iterate array
+  // if current obj at prop 'user' equals given user
+    // push obj value at prop 'message' to userTweets
+// return userTweets
+
+
+var getUserTweets = function(tweets, user) {
+  var userTweets = [];
+  _.each(tweets, function(tweet, index, collection) {
+    if (tweet.user === user) {
+      userTweets.push(tweet);
+    }
+  });
+  return userTweets;
 };
 
 /*
@@ -40,24 +67,62 @@ var getUserTweets = function(tweets, user) {
  */
 
 // use _.filter to return the fruits array with only the desired fruit.
-var onlyOneFruit = function (fruits, targetFruit) {
 
+// input: array of fruits
+// output: array with only the target Fruit
+// constraints: use _.filter
+// edge cases:
+
+// use filter to apply condition that current fruit equals target fruit
+
+var onlyOneFruit = function (fruits, targetFruit) {
+  var onlyFruit = _.filter(fruits, function(fruit) {
+    return fruit === targetFruit;
+  });
+  return onlyFruit;
 };
+console.log(onlyOneFruit(fruits, 'apple'));
 
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
-var startsWith = function (fruits, letter) {
 
+// input: array of fruits, target letter
+// output: array of fruits starting with target letter
+// constraints: use filter
+// edge cases:
+
+var startsWith = function (fruits, letter) {
+  var startsWithLetter = _.filter(fruits, function(fruit) {
+    return fruit[0] === letter;
+  });
+  return startsWithLetter;
 };
 
 // return a filtered array containing only cookie-type desserts.
-var cookiesOnly = function (desserts) {
 
+// input: array of dessert objects
+// output: array of cookie type desserts
+// constraints:
+// edge cases:
+var cookiesOnly = function (desserts) {
+  var cookieDesserts = _.filter(desserts, function(dessert) {
+    return dessert.type === 'cookie';
+  });
+  return cookieDesserts;
 };
 
 // rebuild the getUserTweets function from above with _.filter instead
-var filterUserTweets = function(tweets, user) {
 
+// input: array of objects, string
+// output: array of tweet objects for user
+// constraints: use filter
+// edge cases:
+
+var filterUserTweets = function(tweets, user) {
+  var userTweets = _.filter(tweets, function(tweet) {
+    return tweet.user === user;
+  });
+  return userTweets;
 };
 
 /*
@@ -69,20 +134,54 @@ var filterUserTweets = function(tweets, user) {
 // given an array of strings, use _.map to return a new array containing all
 // strings converted to uppercase letters.
 var upperCaseFruits = function (fruits) {
-
+  return _.map(fruits, function(fruit) {
+    return fruit.toUpperCase();
+  });
 };
 
 // given an array of dessert objects, return a new array of objects
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
-var glutenFree = function (desserts) {
 
+// input: array of dessert objects
+// output: array of objects with new property, 'glutenFree' and corresponding boolean val
+// constraints:
+// edge cases:
+
+// create glutenList
+// iterate dessert objects
+  // add gluten free prop, set value to true
+  // if ingredients array includes 'flour'
+    // set gluten free value to false
+  // push current dessert obj to gluten list
+// return glutenList
+
+
+var glutenFree = function (desserts) {
+  return _.map(desserts, function(dessert) {
+    if (dessert.ingredients.includes('flour')) {
+      dessert.glutenFree = false;
+      console.log(dessert);
+    } else {
+      dessert.glutenFree = true;
+      console.log(dessert);
+    }
+    return dessert;
+  });
 };
 
 // given an array of tweet objects, return a new array of strings
 // containing only the message properties.
-var allUserMessages = function(tweets) {
 
+// input: array of tweet objects
+// output: array of message strings
+// constraints:
+// edge cases:
+
+var allUserMessages = function(tweets) {
+  return _.map(tweets, function(tweet) {
+    return tweet.message;
+  });
 };
 
 // use _.map to return an array of items with their sale prices, with a new property
@@ -105,8 +204,25 @@ var allUserMessages = function(tweets) {
   ];
 
 */
-var applyCoupon = function (groceries, coupon) {
 
+// input: array of grocery objects
+// output: array of items with new prop (salePrice: $price(round to two decimals))
+// constraints:
+// edge cases:
+
+// map array of grocery objs
+  // create discountedPrice var set to price minus (price multiplied by coupon (% off))
+  // creat new prop in grocery called salePrice and equal to discounted price
+  // return grocery obj
+// return mapped array
+
+var applyCoupon = function (groceries, coupon) {
+  return _.map(groceries, function(grocery) {
+    var price = grocery.price.slice(1);
+    var discountedPrice = '$' + (price - (price * coupon)).toFixed(2);
+    grocery.salePrice = discountedPrice;
+    return grocery;
+  });
 };
 
 /*
@@ -116,14 +232,43 @@ var applyCoupon = function (groceries, coupon) {
  */
 
 // return the total price of all products.
-var sumTotal = function (products) {
 
+// input: array of grocery objects
+// output: total price of all products
+// constraints: use redude
+// edge cases:
+
+var sumTotal = function (products) {
+  return _.reduce(products, function(sum, product) {
+    price = parseFloat(product.price.slice(1));
+    return sum + price;
+  }, 0);
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
-var dessertCategories = function (desserts) {
 
+// input: array of dessert objects
+// output: object with how many of each desert type
+// constraints: use reduce
+// edge cases:
+
+// create result obj
+// reduce array
+  // if current dessertType is not defined in result obj
+    // create prop with desert type and set val to 1;
+  // else
+    // increment val by 1
+// return obj
+
+var dessertCategories = function (desserts) {
+  return _.reduce(desserts, function(dessertCounts, dessert) {
+    if (dessertCounts[dessert.type] === undefined) {
+      dessertCounts[dessert.type] = 0;
+    }
+    dessertCounts[dessert.type]++;
+    return dessertCounts;
+  }, {});
 };
 
 // return an object with the proper count of all user messages
@@ -137,20 +282,81 @@ var dessertCategories = function (desserts) {
     "sharksforcheap": 3
   }
 */
-var countMessagesPerUser = function(tweets) {
 
+// input: array of tweet objects
+// output: object with usernames as keys and number of tweets as values
+// constraints: use reduce
+// edge cases:
+
+// create result obj
+// iterate the given array of objects
+  // if current obj username in result obj is undefined
+    // add username to result obj as the key and set value equal to 1
+  // else
+    // increment the username count
+// return result obj
+
+var countMessagesPerUser = function(tweets) {
+  // var tweetCounts = {};
+  // _.reduce(tweets, function(memo, tweet) {
+  //   if (tweetCounts[tweet.user] === undefined) {
+  //     tweetCounts[tweet.user] = 1;
+  //   } else {
+  //     tweetCounts[tweet.user]++;
+  //   }
+  // }, 0);
+  // return tweetCounts;
+  return _.reduce(tweets, function(accumulator, tweet) {
+    if (!accumulator[tweet.user]) {
+      accumulator[tweet.user] = 0;
+    }
+    accumulator[tweet.user] += 1;
+    return accumulator;
+  }, {});
 };
 
 // given an array of movie data objects,return an array containing
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
-var ninetiesKid = function (movies) {
 
+// input: array of movie objects
+// output: array of movies released between 1990 and 2000
+// constraints: use array as accumulator
+// edge cases:
+
+// iterate array of movies
+  // if current movie's release date is >= to 1990 and <= 2000
+    // push movie object to accumulator array
+
+var ninetiesKid = function (movies) {
+  return _.reduce(movies, function(ninetiesMovies, movie) {
+    if (movie.releaseYear >= 1990 && movie.releaseYear <= 2000) {
+      ninetiesMovies.push(movie.title);
+    }
+    return ninetiesMovies;
+  }, []);
 };
 
 // return an boolean stating if there exists a movie with a shorter
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
-var movieNight = function (movies, timeLimit) {
 
+// input: array of movie objects, integer (timeLimit in minutes)
+// output: boolean value, if there is at least one movie that is equal or shorter than timeLimit
+// constraints: use reduce
+// edge cases:
+
+
+// use reduce to iterate movies array, set accumulator value to false
+  // if current movie run time is equal or less than timeLimit
+    // update acccumulator to true
+  // return accumulator
+
+var movieNight = function (movies, timeLimit) {
+  return _.reduce(movies, function(enoughTime, movie) {
+    if (movie.runtime <= timeLimit) {
+      return enoughTime = true;
+    }
+    return enoughTime;
+  }, false);
 };
